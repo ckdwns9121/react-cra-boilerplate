0fbc807e2e314c0251397110e9025963575bfe0f
ChangjunPark <ckdwns9121@naver.com>
2022-07-20 14:27:42 +0900

    # 구조 추가

---
+++ src/components/todo/TodoItem.tsx
@@ -0,0 +1,10 @@
+import type { ITodoItem } from "../../types/todo";
+
+export default function TodoItem({ id, title, content }: ITodoItem) {
+  return (
+    <div className="todo-item">
+      <div className="todo-title">{title}</div>
+      <div className="todo-content">{content}</div>
+    </div>
+  );
+}
---
+++ src/components/todo/TodoList.tsx
@@ -0,0 +1,15 @@
+import type { ITodoItem } from "../../types/todo";
+import TodoItem from "./TodoItem";
+
+interface ITodoList {
+  todos: ITodoItem[];
+}
+export default function TodoList({ todos }: ITodoList) {
+  return (
+    <div className="todo-list">
+      {todos.map(todo => (
+        <TodoItem key={todo.id} id={todo.id} title={todo.title} content={todo.content} />
+      ))}
+    </div>
+  );
+}
