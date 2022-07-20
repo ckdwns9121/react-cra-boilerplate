87dc6679002ba76b8c8cdd5f2448eddd900593c8
ChangjunPark <ckdwns9121@naver.com>
2022-07-20 14:54:20 +0900

    # throttle 추가

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -7,11 +7,11 @@ function useScroll() {
   const [scrollEnd, setScrollEnd] = useState<boolean>(false);
 
   const handleScroll = useCallback(() => {
-    // 화면에 보이지 않은 높이도 포함된 페이지의 총 높이.
+    /**화면에 보이지 않은 높이도 포함된 페이지의 총 높이. */
     const scrollHeight: number = document.documentElement.scrollHeight;
-    // 이미 스크롤 되어서 보이지 않은 구간의 높이.
+    /**이미 스크롤 되어서 보이지 않은 구간의 높이. */
     const scrollTop: number = document.documentElement.scrollTop;
-    // 사용자에게 보여지는 페이지의 높이.
+    /** 사용자에게 보여지는 페이지의 높이.*/
     const clientHeight: number = document.documentElement.clientHeight;
     setScrollEnd(scrollTop + clientHeight >= scrollHeight);
   }, []);
