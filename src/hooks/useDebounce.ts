c5402d08d037526df85f80cf6588b410d86671a0
ChangjunPark <ckdwns9121@naver.com>
2022-07-20 15:08:02 +0900

    # debounce 추가

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+/**
+ *
+ * @param value : input value
+ * @param delay : 몇초뒤 API요청을 할 지
+ * @example
+ * ```ts
+ * import useDebounced from "@hooks";
+ * const debounceSearchTerm = useDebounced(inputValue, 500);
+ *  ...
+ *  useEffect(()=>{
+ *      callApi(debounceSearchTerm)
+ *  },[debounceSearchTerm])
+ * ```
+ * @returns
+ */
+
+function useDebounced(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounced;
