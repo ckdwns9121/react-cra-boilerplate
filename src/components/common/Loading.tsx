1e7f8667f746160874006d4918f6ef4bf6eed325
ChangjunPark <ckdwns9121@naver.com>
2023-03-30 14:20:44 +0900

    # 로딩상태 추가

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,19 +1,18 @@
+import ReactDom from "react-dom";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
-import { useAppSelector } from "../../hooks/useStore";
-import useLoading from "../../hooks/useLoading";
-import ReactDom from "react-dom";
+import { useRecoilState } from "recoil";
+import loadingState from "../../recoil/loding";
 
 const el = document.getElementById("loading") as HTMLElement;
 export default function SimpleBackdrop() {
-  const { open } = useAppSelector((state) => state.loading);
-  const { handleClose } = useLoading();
+  const [loading, setLoading] = useRecoilState(loadingState);
+
+  const handleClose = () => {
+    setLoading(false);
+  };
   return ReactDom.createPortal(
-    <Backdrop
-      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={open}
-      onClick={handleClose}
-    >
+    <Backdrop sx={{ color: "#fff", zIndex: theme => theme.zIndex.drawer + 1 }} open={loading} onClick={handleClose}>
       <CircularProgress color="inherit" />
     </Backdrop>,
     el
